f1c3c8d63f27b2fb3132f4fd737af706ba7ba219
Ankit Ojha <ankitjh521@gmail.com>
2025-08-15 23:35:48 +0530

    simulating multiple requests in a server

--- Program.cs
+++ Program.cs
@@ -1,1 +1,39 @@
-﻿Console.WriteLine("Find code respective to each topic in corresponding topic branches");
+﻿Queue<string?> requestsQueue = new Queue<string?>();
+
+//2. Start the requests queue monitoring thread
+Thread monitoringThread = new Thread(MonitorQueue);
+monitoringThread.Start();   
+
+//1. Enqueue the requests
+Console.WriteLine("Server is running. Type 'exit' to stop.");
+
+while (true)
+{
+   string? input = Console.ReadLine();  
+    if(input?.ToLower() == "exit")
+    {
+        break;
+    }
+
+    requestsQueue.Enqueue(input);
+}
+
+void MonitorQueue()
+{
+    while (true)
+    {
+        if(requestsQueue.Count > 0)
+        {
+            string? input = requestsQueue.Dequeue();
+            Thread processeingThread = new Thread(() => ProcessInput(input));
+            processeingThread.Start();
+            Thread.Sleep(100);
+        }
+    }
+}
+
+static void ProcessInput(string? input)
+{
+    Thread.Sleep(1000);
+    Console.WriteLine($"Processed input: {input}");
+}
