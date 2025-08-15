2de039d08f9253f7a3e09667ec01ecf42c0f5d28
Ankit Ojha <ankitjh521@gmail.com>
2025-08-15 17:22:23 +0530

    basic syntax of thread & setting priority

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿void WriteThreadName()
+{
+    for(int i = 0; i < 100; i++)
+    {
+        Console.WriteLine(Thread.CurrentThread.Name);
+    }
+}
+
+Thread thread1 = new Thread(WriteThreadName);
+Thread thread2 = new Thread(WriteThreadName);
+
+thread1.Name = "Thread 1";
+thread2.Name = "Thread 2";
+Thread.CurrentThread.Name = "Main Thread";
+
+// Setting priority
+thread1.Priority = ThreadPriority.Highest;
+thread2.Priority = ThreadPriority.Normal;
+Thread.CurrentThread.Priority = ThreadPriority.Lowest;
+
+thread1.Start();
+thread2.Start();
+WriteThreadName(); // executes on the main thread
+
+Console.ReadLine();
