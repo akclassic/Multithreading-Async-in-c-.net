92f7755784532db491712c2bd1e9018d4bc866ba
Ankit Ojha <ankitjh521@gmail.com>
2025-08-16 00:27:45 +0530

    synchronization using lock

--- Program.cs
+++ Program.cs
@@ -1,1 +1,25 @@
-﻿Console.WriteLine("Find code respective to each topic in corresponding topic branches");
+﻿int counter = 0;
+
+object counterLock = new object();
+
+Thread thread1 = new Thread(IncrementCounter);
+Thread thread2 = new Thread(IncrementCounter);
+
+thread1.Start();
+thread2.Start();
+
+thread1.Join();
+thread2.Join();
+
+Console.WriteLine($"Final value of the Counter: {counter}");
+
+void IncrementCounter()
+{
+    for (int i = 0; i < 1000; i++)
+    {
+        lock (counterLock) // A lock is used to ensure that only one thread can access the counter at a time
+        {
+            counter++;
+        }
+    }
+}   
