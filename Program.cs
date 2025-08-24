d462c2eeaa17019ed7e898728f9465fe5d46bf6f
Ankit Ojha <ankitjh521@gmail.com>
2025-08-25 00:01:07 +0530

    read and writer lock

--- Program.cs
+++ Program.cs
@@ -1,1 +1,36 @@
-﻿Console.WriteLine("Find code respective to each topic in corresponding topic branches");
+﻿public class GlobalReaderWriterLock
+{
+    private static readonly ReaderWriterLockSlim _lock = new(System.Threading.LockRecursionPolicy.NoRecursion);
+    public Dictionary<string, string> cache { get; set;  } = new Dictionary<string, string>();
+
+    public void AddToCache(string key, string value)
+    {
+        bool isLockAcquired = false;
+        try
+        {
+            _lock.EnterWriteLock();
+            isLockAcquired = true;
+            cache[key] = value;
+        }
+        finally
+        {
+            _lock.ExitWriteLock();
+        }
+    }
+
+    public string? GetFromCache(string key)
+    {
+        bool isLockAcquired = false;
+        try
+        {
+            _lock.EnterReadLock();
+            isLockAcquired = true;
+            cache.TryGetValue(key, out var value);
+            return value ?? null;
+        }
+        finally
+        {
+            _lock.ExitReadLock();
+        }   
+    }
+}
