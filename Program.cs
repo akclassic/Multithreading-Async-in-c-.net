1709991e6a046d7782bff5f509a60e1c3a5882fd
Ankit Ojha <ankitjh521@gmail.com>
2025-09-09 22:11:01 +0530

    added example for manual reset and signaling

--- Program.cs
+++ Program.cs
@@ -1,1 +1,30 @@
-﻿Console.WriteLine("Find code respective to each topic in corresponding topic branches");
+﻿ManualResetEventSlim manualResetEvent = new ManualResetEventSlim(false);
+
+//manualResetEvent.Set();
+
+//manualResetEvent.Reset();
+Console.WriteLine("Press Enter to release all threads");
+
+//Worker THreads
+for(int i = 0; i < 3; i++)
+{
+    Thread thread = new Thread(Work);
+    thread.Name = $"Worker Thread {i + 1}";
+    thread.Start();
+}
+
+Console.ReadLine();
+
+manualResetEvent.Set();
+
+Console.ReadLine();
+
+void Work()
+{
+    Console.WriteLine($"{Thread.CurrentThread.Name} is waiting for the signal");
+    manualResetEvent.Wait();
+
+    Thread.Sleep(2000); // Simulate work
+    Console.WriteLine($"{Thread.CurrentThread.Name} has received the signal and released");
+}
+
